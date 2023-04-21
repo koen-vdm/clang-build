70858119d934c8b5ad8b3813187d98b9c1954694
koenvdm <koen.van-den-maagdenberg.xn@renesas.com>
2023-04-21 12:40:43 +0000

    remove hardcoded user

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-docker build . -t koenvdm/clang-build-tools
+read -p "Enter the docker user to deploy to: " user
+
+docker build . -t "$user/clang-build-tools"
 
 if [[ "$?" -eq 0 ]]; then
-    docker push koenvdm/clang-build-tools
+    docker push "$user/clang-build-tools"
 fi
